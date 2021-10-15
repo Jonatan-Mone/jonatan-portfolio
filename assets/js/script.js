0d541bf8288c54a2904b946f5deab48539b270b8
Jonatan Möne <jonatan.mone@elev.ga.ntig.se>
2021-10-15 13:44:59 +0200

    Made several fixes.

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,38 +43,6 @@ $(function() {
         }
     });
 
-    /* Counters  */
-    if ($(".section-counters .start").length>0) {
-        $(".section-counters .start").each(function() {
-            var stat_item = $(this),
-            offset = stat_item.offset().top;
-            $(window).scroll(function() {
-                if($(window).scrollTop() > (offset - 1000) && !(stat_item.hasClass('counting'))) {
-                    stat_item.addClass('counting');
-                    stat_item.countTo();
-                }
-            });
-        });
-    };
-
-	// another custom callback for counting to infinity
-	$('#infinity').data('countToOptions', {
-		onComplete: function (value) {
-		  count.call(this, {
-		    from: value,
-		    to: value + 1
-		  });
-		}
-	});
-
-	$('#infinity').each(count);
-
-	function count(options) {
-        var $this = $(this);
-        options = $.extend({}, options || {}, $this.data('countToOptions') || {});
-        $this.countTo(options);
-    }
-
     // Navigation overlay
     var s = skrollr.init({
             forceHeight: false,
